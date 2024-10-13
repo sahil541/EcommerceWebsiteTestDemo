8d9f9d486430cb67464d898d88ac846c84b96d06
sahil541 <sahilkaif541@gmail.com>
2024-10-14 00:10:44 +0530

    updated new bin and obj

--- EcommerceWebsiteTest/obj/Debug/net8.0/EcommerceWebsiteTest.AssemblyInfo.cs
+++ EcommerceWebsiteTest/obj/Debug/net8.0/EcommerceWebsiteTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceWebsiteTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa73b1d755402be0ed4e9dc76208687ddf9aa495")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56a0b433cede5c47180d556069e7515c39b1d10")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceWebsiteTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceWebsiteTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
